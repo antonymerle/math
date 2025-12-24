c199071fb853c36d993637b3c38988bf59aed8fc
Antony Merle <antony.merle@gmail.com>
2025-12-24 16:31:44 +0100

    update build script

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 call "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 mkdir ..\..\build\maths
 pushd ..\..\build\maths
-cl /std:c++20 /utf-8 /D_AMD64_ -Zi /EHsc w:\maths\vector.cpp
+cl /std:c++20 /utf-8 /D_AMD64_ -Zi /EHsc w:\maths\main.cpp
 popd
 
-"W:\build\maths\vector.exe"
+"W:\build\maths\main.exe"
