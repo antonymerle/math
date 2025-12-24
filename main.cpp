f1ce42d0ecc1c6c9b8978f96ac9377901f24f4b2
Antony Merle <antony.merle@gmail.com>
2025-12-24 16:30:37 +0100

    entry point

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "vector3D.h"
+#include <iostream>
+
+int main()
+{
+    Vector3D v;
+
+    std::cout << "Hello Mathématiques\n";
+    std::cout << v[0] << " " << v[1] << " " << v[2] << " " << "\n";
+}
