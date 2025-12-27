51ebea7abf61d338bbc2458d404d9ff0579e723a
Antony Merle <antony.merle@gmail.com>
2025-12-27 23:04:10 +0100

    matrix-vector multiplication

--- matrix3D.h
+++ matrix3D.h
@@ -113,6 +113,12 @@ inline Matrix3D operator*(const Matrix3D& A, const Matrix3D& B)
                        A(2,0) * B(0,1) + A(2,1) * B(1,1) + A(2,2) * B(2,1),
                        A(2,0) * B(0,2) + A(2,1) * B(1,2) + A(2,2) * B(2,2)));
 }
-// clang-format on
 
+inline Vector3D operator*(const Matrix3D& M, const Vector3D& v)
+{
+      return Vector3D(M(0, 0) * v.x + M(0, 1) * v.y + M(0, 2) * v.z,
+                      M(1, 0) * v.x + M(1, 1) * v.y + M(1, 2) * v.z,
+                      M(2, 0) * v.x + M(2, 1) * v.y + M(2, 2) * v.z);
+}
+// clang-format on
 #endif
